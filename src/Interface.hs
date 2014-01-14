7d732250c6808f2c90b945d4953bd6ee9625f92b
mrosiew <m.rosiewicz@gmail.com>
2014-01-14 22:36:45 +0100

    added file + gitignore
    git ignore

---
+++ src/Interface.hs
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Interface
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Interface (
+showText text = do
+        putStr text
+) where
+
+
+
